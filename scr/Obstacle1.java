95aa80b796b332b32050ace33b803da257a4ba4d
kennedyromick <60671316+kennedyromick@users.noreply.github.com>
2020-02-25 09:01:49 -0700

    Add files via upload

---
+++ scr/Obstacle1.java
@@ -0,0 +1,23 @@
+
+public class Obstacle1 {
+	
+
+	
+	int row;
+	int column;
+	
+	public Obstacle1(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+	public int getRow() {
+		return row;
+	}
+	
+	public int getColumn() {
+		return column;
+	}
+	
+
+}
