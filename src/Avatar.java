1ab7adba92dde781dc0c77a86257db73733227f4
kennedyromick <60671316+kennedyromick@users.noreply.github.com>
2020-03-08 22:28:24 -0600

    Add files via upload

--- src/Avatar.java
+++ src/Avatar.java
@@ -1,15 +1,13 @@
 package TeamProject;
 
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.shape.Rectangle;
-import javafx.event.ActionEvent;
 
 public class Avatar extends Rectangle
 
 {
 
 	private double velocityX, velocityY;
+	boolean collision = false;
 	
 	public Avatar(double x, double y, double width, double height) {
 		super(x, y, width, height);
@@ -17,68 +15,34 @@ public class Avatar extends Rectangle
 		velocityY = 0;
 	}
 	
+	public void collisionCheck() {
+		//code for collisions with obstacles should go here
+	}
 	
 	
-	public void actionPerformed(ActionEvent e) {
-		double x = getX();
+	public void moveUp() {
+		velocityY = -5.0;
 		double y = getY();
-		x += velocityX;
 		y += velocityY;
-		setX(x);
 		setY(y);
 	}
-	
-	public void up() {
-		velocityY = -1.5;
-		velocityX = 0.0;
+	public void moveDown() {
+		velocityY = 5.0;
+		double y = getY();
+		y += velocityY;
+		setY(y);
 	}
-	
-	public void down() {
-		velocityY = 1.5;
-		velocityX = 0.0;
+	public void moveLeft() {
+		velocityX = -5.0;
+		double x = getX();
+		x += velocityX;
+		setX(x);
 	}
-	
-	public void left() {
-		velocityX = -1.5;
-		velocityY = 0.0;
-	}
-	
-	public void right() {
-		velocityX = 1.5;
-		velocityY = 0.0;
-	}
-	
-	public void keyTyped(KeyEvent e) {
-	}
-	
-	public void keyPressed(KeyEvent e) {
-		if(e.getCode() == KeyCode.RIGHT) {
-			right();
-		}
-		if(e.getCode() == KeyCode.LEFT) {
-			left();
-		}
-		if(e.getCode() == KeyCode.UP) {
-			up();
-		}
-		if(e.getCode() == KeyCode.DOWN) {
-			down();
-		}
-	}
-	
-	public void keyReleased(KeyEvent e) {
-		if(e.getCode() == KeyCode.RIGHT) {
-			velocityX = 0.0;
-		}
-		if(e.getCode() == KeyCode.LEFT) {
-			velocityX = 0.0;
-		}
-		if(e.getCode() == KeyCode.UP) {
-			velocityY = 0.0;
-		}
-		if(e.getCode() == KeyCode.DOWN) {
-			velocityY = 0.0;
-		}
+	public void moveRight() {
+		velocityX = 5.0;
+		double x = getX();
+		x += velocityX;
+		setX(x);
 	}
 
 
