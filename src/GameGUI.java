c2e819b9ba01dc21fdc64d6a66b13114deada821
kennedyromick <60671316+kennedyromick@users.noreply.github.com>
2020-03-08 22:30:51 -0600

    Add files via upload
    Avatar is able to move

--- src/GameGUI.java
+++ src/GameGUI.java
@@ -1,17 +1,14 @@
 package TeamProject;
 
 import javafx.application.Application;
+
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
-import javafx.util.Duration;
-import javafx.animation.AnimationTimer;
-import javafx.animation.Timeline;
+
 
 public class GameGUI extends Application{
 	
@@ -46,6 +43,9 @@ public class GameGUI extends Application{
 		Scene theScene = new Scene(root);
 		primaryStage.setScene(theScene);
 		
+		AvatarMovement avatarHandler = new AvatarMovement(avatar);
+		theScene.setOnKeyPressed(avatarHandler);
+		
 		Canvas canvas = new Canvas(1350,800);
 		GraphicsContext gc = canvas.getGraphicsContext2D();
 		root.getChildren().add(canvas);
@@ -57,17 +57,5 @@ public class GameGUI extends Application{
 		primaryStage.show();
 	}
 	
-	//this is where I was trying to set up the timer, I'm not 100% sure if 
-	//the code for the timer should go in the class or the Avatar class
-	//you can even create a new Moveable class if that made it easier
-//	public void startGame() {
-//		AnimationTimer gameLoop = new AnimationTimer() {
-//			@Override
-//			public void handle(long now) {
-//				//I can't figure out what should be in this method
-//			}
-//		};
-//		gameLoop.start();
-//	}
 
 }
