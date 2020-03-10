83491ba24d74afeddcdbcd8e4cc7a9d17cae6f0e
kennedyromick <60671316+kennedyromick@users.noreply.github.com>
2020-03-09 22:17:26 -0600

    Add files via upload
    GameGUI with collisionCheck()

--- src/GameGUI.java
+++ src/GameGUI.java
@@ -10,6 +10,7 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -20,13 +21,13 @@ public class GameGUI extends Application{
 
 	ArrayList<Obstacle> obstacleList = new ArrayList<Obstacle>();
 	
-	Avatar avatar = new Avatar(0,800,50,50);
-	Obstacle one = new Obstacle(100,800,200,50);
+	Avatar avatar = new Avatar(0,765,35,35);
+	Obstacle one = new Obstacle(0,800,1350,50);
 	Obstacle two = new Obstacle(300,725,200,50);
 	Obstacle three = new Obstacle(500,650,100,50);
 	Obstacle four = new Obstacle(600,725,200,50);
 	Obstacle five = new Obstacle(250,575,200,50);
-	Obstacle six = new Obstacle(800,800,200,50);
+	//Obstacle six = new Obstacle(800,800,200,50);
 	Obstacle seven = new Obstacle(150,500,100,50);
 	Obstacle eight = new Obstacle(0,425,150,50);
 	Obstacle nine = new Obstacle(650,575,200,50);
@@ -61,13 +62,13 @@ public class GameGUI extends Application{
 		root.getChildren().add(canvas);
 		
 		avatar.setFill(Color.BLUE);
-		root.getChildren().addAll(avatar, one, two, three, four, five, six, seven, eight, nine);
+		root.getChildren().addAll(avatar, one, two, three, four, five, seven, eight, nine);
 		root.getChildren().addAll(ten, eleven, tweleve, thirteen, fourteen, fifteen, sixteen);
 		
      	createObjList();
      	
      	Timeline timeline = new Timeline(
-     			new KeyFrame(Duration.millis(10),
+     			new KeyFrame(Duration.millis(0.25),
      	               new EventHandler <ActionEvent>()
      				   {
      				   	@Override
@@ -75,8 +76,11 @@ public class GameGUI extends Application{
      				   	{
      						for(Obstacle o: obstacleList) {
      							avatar.collisionCheck(o);
+     							}
+     						// avatar.grav();
+     						
+
      						}
-     				   }
      				   }
      			)
      			);
@@ -94,7 +98,7 @@ public class GameGUI extends Application{
 		obstacleList.add(three);
 		obstacleList.add(four);
 		obstacleList.add(five);
-		obstacleList.add(six);
+		//obstacleList.add(six);
 		obstacleList.add(seven);
 		obstacleList.add(eight);
 		obstacleList.add(nine);
