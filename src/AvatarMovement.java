545c9cb8d8385c807246eac090e7156bb7dd1d89
SeanAnselmo <60671304+SeanAnselmo@users.noreply.github.com>
2020-03-10 14:36:48 -0600

    Add files via upload
    Fixes boundaries

--- src/AvatarMovement.java
+++ src/AvatarMovement.java
@@ -6,7 +6,7 @@ import javafx.scene.input.KeyEvent;
 public class AvatarMovement implements EventHandler<KeyEvent>{
 
 	private Avatar avatar;
-	protected boolean canJump = true;
+	
 	
 	public AvatarMovement(Avatar avatar) {
 		this.avatar = avatar;
@@ -15,11 +15,11 @@ public class AvatarMovement implements EventHandler<KeyEvent>{
 	@Override
 	public void handle(KeyEvent e) {
 		switch(e.getCode()) {
-		case D: if(avatar.getX() <= 1308) {avatar.moveRight();}
+		case D: if(avatar.getX() <= 1310) {avatar.moveRight();}
 			break;
 		case A: if(avatar.getX() >= 5) {avatar.moveLeft();}
 			break;
-		case W:if(avatar.getY() >= 5) {  avatar.jump();}
+		case W:if(avatar.getY() >= 5) {avatar.jump();}
 			break;
 		case S: avatar.moveDown();
 			break;
