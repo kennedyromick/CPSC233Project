776ce95902b4a0fed9d51ba2da8fb303dc47c85c
kromi <kennedy.romick@ucalgary.ca>
2020-03-20 08:57:53 -0600

    Text based version put into scr--TextBasedVersion

---
+++ scr/TextBasedVersion/Obstacle1.java
@@ -0,0 +1,24 @@
+package CrazyCodingClub.TextBasedVersion;
+
+public class Obstacle1 {
+	
+
+	
+	int row;
+	int column;
+	
+	public Obstacle1(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+	public int getRow() {
+		return row;
+	}
+	
+	public int getColumn() {
+		return column;
+	}
+	
+
+}
