9e40265c37087c3f12c7cb2a747a2ff729ce8db3
kennedyromick <60671316+kennedyromick@users.noreply.github.com>
2020-04-13 13:09:37 -0600

    completed texted based

--- src/TextBasedGameGUI.java
+++ src/TextBasedGameGUI.java
@@ -1,12 +1,5 @@
-import java.util.ArrayList;
-import java.util.Scanner;
 
 public class TextBasedGameGUI {
-	
-
-	static ArrayList<TextBasedObstacle> obstacles = new ArrayList<TextBasedObstacle>();
-	static ArrayList<TextBasedEnemies> enemies = new ArrayList<TextBasedEnemies>();
-	static Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		TextBasedLevelOne.main(args);
